1e8c9d1e07fb6532ac147f4960ae6a4a57e42814
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-07-08 19:49:12 +0900

    initial commit

---
+++ SwiftUI_yamatatsu/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI_yamatatsu
+//
+//  Created by Tatsuya Yamamoto on 2019/7/8.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
